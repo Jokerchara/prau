9061ae1ef5e689952a3f7f2e7777fdeae2caf1a9
Jokerchara <taegds@gmail.com>
2019-07-31 08:55:45 +0700

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                     {{$user->nama}} You are logged in!
                 </div>
             </div>
         </div>
