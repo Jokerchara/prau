eae1fbcd72e0559410fefb94de32882368c8e634
Jokerchara <taegds@gmail.com>
2019-08-01 08:32:28 +0700

    fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Albi N M</title>
+    <title>Albi Nur</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
